b1b576bd7c35fbf3cef67d4099951be8bbc5dea3
Felix Seidel <felix@gnampfelix.de>
2024-01-25 17:25:00 +0100

    Implement window coloring based on uniqueness
    This is only temporary, I will move this part into java code

--- misc/visualize_coordinates.py
+++ misc/visualize_coordinates.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def findGapsLargerThan(sortedList, threshold):
     result = []
@@ -8,6 +9,29 @@ def findGapsLargerThan(sortedList, threshold):
         if delta > threshold:
             result.append((delta, i, sortedList[i]))
     return result
+
+def collectWindowForRow(row, df, window_size):
+    start_pos = row["sequenceIndexInFile"]
+    window = df[(df["sequenceIndexInFile"] < start_pos + window_size) & (df["sequenceIndexInFile"] >= start_pos)]
+    size = len(window)
+    counts = window["kmer"].value_counts()
+    unique_ratio = len(counts[counts == 1]) / size
+    return (start_pos, size, unique_ratio)
+
+def collectWindowsForDf(sorted_df, window_size):
+    rows = []
+    for i in range(len(sorted_df)):
+        current_row = sorted_df.iloc[i, :]
+        window_start = current_row["sequenceIndexInFile"]
+        kmers = [current_row["kmer"]]
+        j = i + 1
+        while j < len(sorted_df) and sorted_df.iloc[j, :]["sequenceIndexInFile"] - window_start < window_size:
+            kmers.append(sorted_df.iloc[j, :]["kmer"])
+            j += 1
+        unique_kmers = [kmer for kmer in kmers if kmers.count(kmer) == 1]
+        current_window = [window_start, len(kmers), len(unique_kmers)/len(kmers)]
+        rows.append(current_window)
+    return pd.DataFrame(rows, columns=["window_start", "kmer_count", "unique_kmer_ratio"])
 
 def collectWindows(sorted_list, window_size):
     result = []
@@ -27,16 +51,22 @@ def findDensestWindows(window_list, threshold):
             result.append((window[0], len(window)))
     return result
 
-pos = []
-pos_ambig = []
-with open("fmhdist/PhyInf1.fna.gz.sketch.coordinates", "r") as f:
-    for line in f.readlines():
-        parts = line.split(":")
-        current = []
-        for part in parts:
-            current.append(part.split(",")[1].replace(")", "").strip())
-        pos.append(current[0])
-        pos_ambig.append(current[1])
+df = pd.read_csv("fmhdist/PhyInf1.fna.gz.sketch.coordinates", names=[
+    "recordIndex",
+    "sequenceIndexInFile",
+    "sequenceIndexInRecord",
+    "sequenceIndexInFileIncludingAmbiguous",
+    "sequenceIndexInRecordIncludingAmbiguous",
+    "kmer"
+    ])
+
+df["kmer"] = df["kmer"].astype("string")
+counts = df["kmer"].value_counts()
+df["counts"] = df.apply(lambda row: counts[row["kmer"]],axis=1)
+
+pos = df["sequenceIndexInFile"].to_numpy()
+pos_ambig = df["sequenceIndexInFileIncludingAmbiguous"].to_numpy()
+counts = df["counts"].to_numpy()
 
 window_size = 10000
 density_threshold = 20
@@ -44,22 +74,21 @@ gap_threshold = 20000
 pos = np.array(pos, dtype=np.uint)
 pos_ambig = np.array(pos_ambig, dtype=np.uint)
 
-windows = collectWindows(pos_ambig, window_size)
-
+windows = collectWindowsForDf(df, window_size)
 
 largerThanT = findGapsLargerThan(pos_ambig, gap_threshold)
 max_gap_size = np.max([size for size, _, _ in largerThanT])
 min_gap_size = np.min([size for size, _, _ in largerThanT])
 
-densestWindows = findDensestWindows(windows, density_threshold)
-densestWindowsMaxDensity = np.max([size for _, size in densestWindows])
-densestWindowsMinDensity = np.min([size for _, size in densestWindows])
+densestWindows = windows[windows["kmer_count"] > density_threshold]
+densestWindowsMaxDensity = np.max(densestWindows["kmer_count"].to_numpy())
+densestWindowsMinDensity = np.min(densestWindows["kmer_count"].to_numpy())
 
 plt.scatter(pos, len(pos) * [0], s=1, c="blue", label="hash origin position excluding ambiguous $k$-mers")
 plt.scatter(pos_ambig, len(pos_ambig) * [0.01], s=1, c="orange", label="hash origin position including ambigouos $k$-mers")
 
 plt.scatter([pos for _, _, pos in largerThanT], len(largerThanT)* [0.11], s=[(size-min_gap_size)/(max_gap_size-min_gap_size) * 100 for size, _, _ in largerThanT], c="green", label=f"gaps larger than $t={gap_threshold}$")
-plt.scatter([pos for pos, _ in densestWindows], len(densestWindows)*[-0.11], s=[(density-densestWindowsMinDensity)/(densestWindowsMaxDensity-densestWindowsMinDensity) * 100 for _, density in densestWindows], c="black", label=f"windows with size $s={window_size}$ that are denser than $t={density_threshold}$")
+plt.scatter(densestWindows["window_start"], len(densestWindows)*[-0.11], s=(densestWindows["kmer_count"].to_numpy()-densestWindowsMinDensity)/(densestWindowsMaxDensity-densestWindowsMinDensity) * 100, c=densestWindows["unique_kmer_ratio"], label=f"windows with size $s={window_size}$ that are denser than $t={density_threshold}$")
 
 plt.xlim(0, np.max(pos_ambig))
 plt.ylim(-1, 1)
