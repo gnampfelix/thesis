068b18e7428e09417410802c536797c613dcbee0
Felix Seidel <felix@gnampfelix.de>
2024-04-26 17:24:59 +0200

    Create script to calculate all empty intersections of a set of mash sketches

---
+++ misc/get_empty_intersections.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import argparse
+import json
+
+def create_parser():
+    p = argparse.ArgumentParser(description=__doc__,
+                                formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    p.add_argument('-i', '--input',
+                   help="Path to the json exports of the mash sketches", nargs="+")
+
+    return (p.parse_args())
+
+def main():
+    args = create_parser()
+    sketches = []
+    for path in args.input:
+        with open(path) as f:
+            sketch = json.load(f)
+            sequence_name = sketch["sketches"][0]["name"].split("/")[-1]
+            hashes = set(sketch["sketches"][0]["hashes"])
+            sketches.append((sequence_name, hashes))
+
+    for i in range(len(sketches)):
+        for j in range(i, len(sketches)):
+            name_a, a = sketches[i]
+            name_b, b = sketches[j]
+            interesction = a.intersection(b)
+            if len(interesction) == 0:
+                print(f"{name_a} vs {name_b} is empty")
+
+
+if __name__ == "__main__":
+    main()
